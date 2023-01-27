300193cf5c6d8f7574e74351546c33feaaadf5d8
hymetacore <hymetacore@hymetacores-Mac-mini.local>
2023-01-26 16:40:36 -0800

    completely finished

--- src/script.js
+++ src/script.js
@@ -1,38 +1,36 @@
+window.onload = function () {
+  const input = document.getElementsByTagName("input")[0];
+  let resultArr = ["UP⬆️", "DOWN⬇️", "CORRECT!🥳"];
+  let result = document.querySelector(".result");
+  let randomNum = getRandomNum(50);
 
+  const button = document.querySelector(".startBtn");
+  button.addEventListener("click", function () {
+    resultFunc();
+  });
 
-window.onload() = function(){
-   const input = document.getElementsByTagName("input")[0];
-   let resultArr = ["UP⬆️", "DOWN⬇️", "CORRECT!🥳" ]
-   let result = document.querySelector(".result");
-   let randomNum = randomNum(50);
-   
-   const button = document.querySelector(".startBtn");
-    button.addEventListener("click", function(){
-        resultFunc();
-    });
-    
-    function resultFunc() {
-        let value = input.value;
-        if(value < randomNum){
-            result.innerHTML = resultArr[0];
-        } else if (value > randomNum){
-            result.innerHTML = resultArr[1];
-        } else{
-            result.innerHTML = resultArr[2];
-            //after 2sec
-            setTimeout(function(){
-                resetFunc();
-            }, 3000)
-        }
+  function resultFunc() {
+    let value = input.value;
+    if (value < randomNum) {
+      result.innerHTML = resultArr[0];
+    } else if (value > randomNum) {
+      result.innerHTML = resultArr[1];
+    } else {
+      result.innerHTML = resultArr[2];
+      //after 2sec
+      setTimeout(function () {
+        resetFunc();
+      }, 3000);
     }
+  }
 
-    function resetFunc(){
-        input.value =  1;
-        randomNum = randomNum(50);
-        result.innerHTML = "One More Game! ( 1 ~ 50 )"
-    }
+  function resetFunc() {
+    input.value = 1;
+    randomNum = getRandomNum(50);
+    result.innerHTML = "One More Game! ( 1 ~ 50 )";
+  }
 
-    function randomNum() {
-        return Math.ceil(Math.random()*num);
-    }
-}
+  function getRandomNum(num) {
+    return Math.ceil(Math.random() * num);
+  }
+};
